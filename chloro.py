7a270bf4a71cb4f3633be2b6b14eead1b5604ad5
Rolin Buckoke <rbuckoke@uvic.ca>
2020-03-30 21:24:17 -0700

    making the stuff less repetitive

--- chloro.py
+++ chloro.py
@@ -54,121 +54,38 @@ app.layout = html.Div([
     [Input('radio-buttons', 'value')]
 )
 
-def update_figure(category):        
-    if category == 0 :
-        fig = go.Figure(data=go.Choropleth(
+def update_figure(category):  
+
+    categories = ([
+        {'title':'2018 World Happiness Score', 'color': 'blues', 'data':'Happiness score'},
+        {'title': '2018 GDP per capita', 'color': 'icefire', 'data':'Log GDP per capita'},
+        {'title':'2018 World Happiness Score', 'color': 'blues', 'data':'Happiness score'},
+        {'title': '2018 GDP per capita', 'color': 'icefire', 'data':'Log GDP per capita'},
+        {'title':'2018 World Happiness Score', 'color': 'blues', 'data':'Happiness score'},
+    
+    ])
+    fig = go.Figure(data=go.Choropleth(
         locations = df['CODE'],
         text = df['Country'],
-        colorscale = 'blues',
+        colorscale = categories[category]['color'],
         autocolorscale=False,
         marker_line_color='darkgray',
         marker_line_width=0.5,
-        z = df['Happiness score'],
+        z = df[categories[category]['data']],
         reversescale=True,
-        colorbar_title = 'Happiness Score',
+        colorbar_title = categories[category]['title'],
         ))
 
-        fig.update_layout(
-            title_text='2018 Happiness Scores',
-            geo=dict(
-                showframe=False,
-                showcoastlines=False,
-                projection_type='equirectangular'
-            ),
-            
-        )
-    elif category == 1:
-        fig = go.Figure(data=go.Choropleth(
-        locations = df['CODE'],
-        text = df['Country'],
-        colorscale = 'edge',
-        autocolorscale=False,
-        marker_line_color='darkgray',
-        marker_line_width=0.5,
-        z = df['Log GDP per capita'],
-        reversescale=True,
-        colorbar_title = 'Log GDP',
-        ))
-
-        fig.update_layout(
-            title_text='2018 Log GDP per capita',
-            geo=dict(
-                showframe=False,
-                showcoastlines=False,
-                projection_type='equirectangular'
-            ),
-            
-        )
-
-    elif category == 2:
-        fig = go.Figure(data=go.Choropleth(
-        locations = df['CODE'],
-        text = df['Country'],
-        colorscale = 'edge',
-        autocolorscale=False,
-        marker_line_color='darkgray',
-        marker_line_width=0.5,
-        z = df['Freedom to make life choices'],
-        reversescale=True,
-        colorbar_title = 'Freedom to make life choices',
-        ))
-
-        fig.update_layout(
-            title_text='2018 Freedom to make life choices',
-            geo=dict(
-                showframe=False,
-                showcoastlines=False,
-                projection_type='equirectangular'
-            ),
-            
-        )
-
-    elif category == 3:
-        fig = go.Figure(data=go.Choropleth(
-        locations = df['CODE'],
-        text = df['Country'],
-        colorscale = 'icefire',
-        autocolorscale=False,
-        marker_line_color='darkgray',
-        marker_line_width=0.5,
-        z = df['Generosity'],
-        reversescale=True,
-        colorbar_title = 'Generosity',
-        ))
-
-        fig.update_layout(
-            title_text='2018 Generosity',
-            geo=dict(
-                showframe=False,
-                showcoastlines=False,
-                projection_type='equirectangular'
-            ),
-            
-        )
-
-    elif category == 4:
-        fig = go.Figure(data=go.Choropleth(
-        locations = df['CODE'],
-        text = df['Country'],
-        colorscale = 'edge',
-        autocolorscale=False,
-        marker_line_color='darkgray',
-        marker_line_width=0.5,
-        z = df['Social support'],
-        reversescale=True,
-        colorbar_title = 'Social Support',
-        ))
-
-        fig.update_layout(
-            title_text='2018 Social Support',
-            geo=dict(
-                showframe=False,
-                showcoastlines=False,
-                projection_type='equirectangular'
-            ),
-            
-        )
+    fig.update_layout(
+        title_text=categories[category]['title'],
+        geo=dict(
+            showframe=False,
+            showcoastlines=False,
+            projection_type='equirectangular'
+        ),
+    )
     return fig
+
 
 
 app.run_server(debug=True, dev_tools_hot_reload=True, use_reloader=False,)
